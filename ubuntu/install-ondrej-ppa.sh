0628fc107b1a6db9545085aac7f18968e496605e
Vishal Doshi <vishal.doshi@gmail.com>
2015-04-05 19:55:50 +0530

    Misc cleanup and robustness (install-ondrej-ppa)

--- ubuntu/install-ondrej-ppa.sh
+++ ubuntu/install-ondrej-ppa.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-apt-get install -y software-properties-common
+set -e -o nounset 
+
+apt-get install -y software-properties-common python-software-properties
 add-apt-repository ppa:ondrej/php5-5.6
