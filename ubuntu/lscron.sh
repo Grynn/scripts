0f717efd3198de3bbb2f45efdc759ec47ce5d0bd
Vishal Doshi <vishal.doshi@gmail.com>
2020-02-22 08:09:50 +0530

    Random small update to  lscron

--- ubuntu/lscron.sh
+++ ubuntu/lscron.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #List all cron jobs for all users
 
-set -e
+set -e -x
 
 for user in `cat /etc/passwd | cut -d":" -f1`;
 do
-  sudo crontab -l -u $user 2>&1 | egrep -v "no crontab.*"
+  crontab -l -u $user 2>&1 | egrep -v "no crontab.*"
 done
