1bd5b258cc7f781aaef7728c492b704f26469812
Vishal Doshi <vishal.doshi@gmail.com>
2021-07-07 05:17:55 +0530

    Minor mods to lscron

--- ubuntu/lscron.sh
+++ ubuntu/lscron.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #List all cron jobs for all users
 
-set -e -x
+set -e 
 
 for user in `cat /etc/passwd | cut -d":" -f1`;
 do
-  crontab -l -u $user 2>&1 | egrep -v "no crontab.*"
+  echo "Cron for $user"
+  crontab -l -u $user 2>&1 | egrep -v "no crontab.*" | egrep -v "^#"
 done
